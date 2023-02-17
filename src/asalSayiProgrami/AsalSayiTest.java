05fb7024d754ca1fcc6cfbf4b9ab20fcdceb9009
arwen <arwen@localhost>
2023-02-18 01:09:24 +0300

    www.patika.dev

---
+++ src/asalSayiProgrami/AsalSayiTest.java
@@ -0,0 +1,27 @@
+package asalSayiProgrami;
+
+
+
+public class AsalSayiTest {
+
+	public static void main(String[] args) {
+	int i;
+	int j;
+	
+	
+		for( i=2; i<=100; i++) {
+		 int counter=0;
+		 
+			for( j=1; j<=i; j++ ) {
+			
+			if (i%j == 0) {
+			counter++;
+			
+	}
+		    
+		} if (counter <=2) {
+		    	System.out.println(i);
+		    }
+ }
+		}
+}
